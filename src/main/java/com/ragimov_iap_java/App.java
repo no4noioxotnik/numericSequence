12b79faed223388766dfdd9586ef1b518d9c5ea0
Rustam Ragimov <rustam_ragimov@epam.com>
2018-02-20 13:13:55 +0300

    add README

---
+++ src/main/java/com/ragimov_iap_java/App.java
@@ -0,0 +1,13 @@
+package com.ragimov_iap_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Helloxcx  World!" );//ddsd
+    }
+}
