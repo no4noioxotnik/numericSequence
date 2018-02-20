5c24f0612b57108dccd823920e8dc3e72f383462
Rustam Ragimov <rustam_ragimov@epam.com>
2018-02-20 19:02:16 +0300

    adding file

--- src/main/java/com/ragimov_iap_java/SDifference.java
+++ src/main/java/com/ragimov_iap_java/SDifference.java
@@ -4,9 +4,7 @@ import java.util.*;
 
 import static java.util.Arrays.asList;
 
-/**
- * Created by Rustam_Ragimov on 2/20/2018.
- */
+
 public class SDifference {
 
     public static <T> Collection<T> symmetricDifference(Collection<T> a, Collection<T> b) {
@@ -14,36 +12,36 @@ public class SDifference {
         intersect.retainAll(b);
         Collection<T> result = new ArrayList<T>(a);
         result.addAll(b);
-        result.removeAll(intersect);//deleting elements from both collections
+        result.removeAll(intersect);//deleting common elements for both collections
         return result;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String multFirst = "Первое множество: ";
-        String multSecond = "Второе множество: ";
+        String multFirst = "First group: ";
+        String multSecond = "Second group: ";
 
-        System.out.println(multFirst + "Введите первое число");
+        System.out.println(multFirst + "Type the first number");
         int a = sc.nextInt();
 
-        System.out.println(multFirst + "Введите второе число");
+        System.out.println(multFirst + "Type the second number");
         int b = sc.nextInt();
 
-        System.out.println(multFirst + "Введите третье число");
+        System.out.println(multFirst + "Type the third number");
         int c = sc.nextInt();
 
-        System.out.println(multSecond + "Введите первое число");
+        System.out.println(multSecond + "Type the first number");
         int d = sc.nextInt();
 
-        System.out.println(multSecond + "Введите второе число");
+        System.out.println(multSecond + "Type the second number");
         int e = sc.nextInt();
 
-        System.out.println(multSecond + "Введите третье число");
+        System.out.println(multSecond + "Type the third number");
         int f = sc.nextInt();
 
         Collection<String> first = new ArrayList(asList(a, b, c));
         Collection<String> second = new ArrayList(asList(d, e, f));
         Collection<String> fin = symmetricDifference(first,second);
-        System.out.println(Arrays.toString(fin.toArray()));
+        System.out.println("The Symmetric Difference: " + Arrays.toString(fin.toArray()));
     }
 }
